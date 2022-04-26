0a56cef04fa348449a9ebdc39a646bea8f8dd551
Leonardo Zamarchi <leonardozamarchi@gmail.com>
2022-04-26 13:26:15 -0300

    Create odbc_python.py

---
+++ odbc_python.py
@@ -0,0 +1,29 @@
+import pyodbc 
+import env
+import os
+import pandas as pd
+
+username = os.environ['USERNAME']  
+password = os.environ['USER_PWD'] #set a enviroment path to keep your pwd
+
+def conecta_odbc():
+    username = os.environ['USERNAME']  
+    password = os.environ['USER_PWD']
+    server = 'virtualizador.sicredi.net' 
+    database = 'ldw'     
+    con = pyodbc.connect('DRIVER={driver_name};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+    return con
+
+
+def select(con, columns, table, exception):
+    if len(exception.replace(' ','')) < 1:
+        select = 'SELECT '+columns+' from '+table
+    else:
+        select = 'SELECT '+columns+' from '+table+ ' where '+exception
+    
+    table = pd.read_sql(select,con)
+    df = pd.DataFrame(table)
+
+    return df
+    
+    
