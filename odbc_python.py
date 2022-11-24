e7ffd08ee049097c9b2c1ab5f2798c06eaafae11
Leonardo Zamarchi <leonardozamarchi@gmail.com>
2022-11-24 11:10:17 -0300

    Update odbc_python.py

--- odbc_python.py
+++ odbc_python.py
@@ -27,3 +27,46 @@ def select(con, columns, table, condit):
     return df
     
     
+
+def conecta_oracle():    
+    try:
+        con = cx_Oracle.connect('user','pwd','con:1521/xe')
+        cursor = con.cursor()
+        return(con)
+    except cx_Oracle.Error as error:
+        print('Error occurred:')
+        print(error)
+
+
+def select_oracle(con, columns, table, exception):
+    try:
+        if len(exception.replace(' ','')) < 1: 
+            select = 'SELECT '+columns+' from '+table
+        else:
+            select = 'SELECT '+columns+' from '+table+ ' where '+exception
+        
+        table = pd.read_sql(select,con)
+        return(table)
+    except cx_Oracle.Error as error:
+        print('Error occurred:')
+        print(error)
+
+
+
+def insert_oracle(con, df, table):
+    try:
+        col = df.columns.tolist()
+        vrs = ""
+        for i in range(0,len(col)):
+            vrs += ':'+str(i+1)+','
+        cols = []
+        sql = 'INSERT INTO '+table+' VALUES ('+vrs.rstrip(',')+')'
+        for i in range(0,len(df)):
+            cols.append(tuple(df.fillna('').values[i]))
+            print(i)
+        con = conecta_oracle()
+        cursor =  con.cursor()
+        cursor.executemany(sql,cols)
+        con.commit()
+    except cx_Oracle.Error as error:
+        print('Error occurred: '+error)
